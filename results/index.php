c920e732472b6756c08758ef9d2145b564bb3c46
Jon Carr <jwcarr@users.noreply.github.com>
2015-11-27 20:35:50 +0000

    Changed sort to place most recent first
    + more padding in the table

--- results/index.php
+++ results/index.php
@@ -14,12 +14,12 @@ $results = array();
 if ($handle = opendir($data_path)) {
   while (false !== ($entry = readdir($handle))) {
     if (($entry != '.') && ($entry != '..')) {
-      $last_modified = date("Y-m-d H:i", filemtime($data_path . $entry));
+      $last_modified = filemtime($data_path . $entry);
       $results[$last_modified] = $entry;
     }
   }
   closedir($handle);
-  ksort($results);
+  krsort($results);
 }
 
 ?>
@@ -32,16 +32,18 @@ if ($handle = opendir($data_path)) {
 <table>
   <tr>
     <td></td>
-    <td><strong>Time completed</strong></td>
-    <td><strong>Session ID</strong></td>
+    <td style="padding:10px;"><strong>Time completed</strong></td>
+    <td style="padding:10px;"><strong>Session ID</strong></td>
   </tr>
 <?php foreach ($results as $last_modified => $session_id) {
-  echo '  <tr>
-    <td><a href="index.php?download='. $session_id .'"><img src="download.png" height="16" width="16" /></a></td>
-    <td>' . $last_modified . '</td>
-    <td>' . $session_id . '</td>
-  </tr>
-';
+  if ($session_id[0] !== '.') {
+    echo '  <tr>
+      <td style="padding:10px;"><a href="index.php?download='. $session_id .'"><img src="download.png" height="16" width="16" /></a></td>
+      <td style="padding:10px;">' . date("d M Y H:i", $last_modified) . '</td>
+      <td style="padding:10px;">' . $session_id . '</td>
+    </tr>
+  ';
+  }
 } ?>
 </table>
 </body>
